7d9e1ab79a5c983f3ac87de39291ef7692f19269
Daniele Mazzotti <kappei84@gmail.com>
2018-01-31 21:30:14 +0100

    added Jgit Flow for testing purposes; fixed Error Handling when logging in

--- src/main/java/com/aig/crm/shared/config/MvcConfig.java
+++ src/main/java/com/aig/crm/shared/config/MvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -47,6 +48,12 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
         return lci;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/404.html").setViewName("404");
+        registry.addViewController("/403.html").setViewName("403");
     }
 
     @Override
