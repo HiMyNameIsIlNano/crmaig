ede1187c17c71d135af090db86ce356715b88de8
Daniele Mazzotti <kappei84@gmail.com>
2018-03-22 21:01:05 +0100

    removed unused import;

--- src/main/java/com/aig/crm/shared/config/MvcConfig.java
+++ src/main/java/com/aig/crm/shared/config/MvcConfig.java
@@ -4,6 +4,7 @@ import com.aig.crm.breadcrumb.interceptor.BreadCrumbInterceptor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
